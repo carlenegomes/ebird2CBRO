9198424c6d12a974bc7123968ad022a618df648d
carlenegomes <carlene.gomesr@gmail.com>
2026-06-29 21:04:45 -0300

    refactor add_presence_column function to consider only the species lines

--- src/ebird2cbro/cbro.py
+++ src/ebird2cbro/cbro.py
@@ -13,18 +13,23 @@ def load_species_file(species_file):
 
     raise ValueError("O arquivo deve ser .csv, .xlsx ou .xls")
 
-
 def add_presence_column(
     cbro_df,
     species_df,
     column_name,
     cbro_species_col="Nome do táxon (sem autoria)",
-    species_col="scientific_name"
+    species_col="scientific_name",
+    cbro_rank_col="Categoria",
+    species_rank_value="Espécie"
 ):
     species = species_df[species_col].dropna().astype(str).str.strip().unique()
 
-    cbro_df[column_name] = (
-        cbro_df[cbro_species_col]
+    is_species = cbro_df[cbro_rank_col] == species_rank_value
+
+    cbro_df[column_name] = None
+
+    cbro_df.loc[is_species, column_name] = (
+        cbro_df.loc[is_species, cbro_species_col]
         .astype(str)
         .str.strip()
         .isin(species)
