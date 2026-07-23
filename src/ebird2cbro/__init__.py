e80603537e65786c9e70b7623f4886171ccf21c1
carlenegomes <carlene.gomesr@gmail.com>
2026-07-22 21:59:27 -0300

    adicionando exports necessários

--- src/ebird2cbro/__init__.py
+++ src/ebird2cbro/__init__.py
@@ -1,3 +1,14 @@
-
 from .main import ebird2cbro
-from .cbro import match_species
+
+from .cbro import (
+    match_species,
+    load_default_cbro,
+    add_multiple_presence_columns
+)
+
+__all__ = [
+    "ebird2cbro",
+    "match_species",
+    "load_default_cbro",
+    "add_multiple_presence_columns"
+]
